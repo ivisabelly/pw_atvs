16129073acf4f05b866ea01054b36a8e9e7f90cb
Isabelly C. Veras <124399040+ivisabelly@users.noreply.github.com>
2024-04-17 08:40:05 -0300

    Add files via upload

---
+++ projetow/telefone/telefo.js
@@ -0,0 +1,49 @@
+bt1.addEventListener("click", function(){ //o valor do botao vai ser o valor do bttel tb 
+    bttel.value += bt1.value;
+    });
+    
+    bt2.addEventListener("click", function(){ 
+    bttel.value += bt2.value;
+    });
+    
+    bt3.addEventListener("click", function(){ 
+    bttel.value += bt3.value;
+    });
+    
+    bt4.addEventListener("click", function(){
+    bttel.value += bt4.value;
+    });
+    
+    bt5.addEventListener("click", function(){
+    bttel.value += bt5.value;
+    });
+    
+    bt6.addEventListener("click", function(){
+    bttel.value += bt6.value;
+    });
+    
+    bt7.addEventListener("click", function(){
+    bttel.value += bt7.value;
+    });
+    
+    bt8.addEventListener("click", function(){
+    bttel.value += bt8.value;
+    });
+    
+    bt9.addEventListener("click", function(){
+    bttel.value += bt9.value;
+    });
+    
+    bt10.addEventListener("click", function(){
+    bttel.value += bt10.value;
+    });
+    
+    bt11.addEventListener("click", function(){
+    bttel.value += bt11.value;
+    });
+    
+    bt12.addEventListener("click", function(){
+    bttel.value += bt12.value;
+    });
+    
+    
